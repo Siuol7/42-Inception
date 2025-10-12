bc4536e0114e0f67f7b901f2835e41b3cf046a08
Siuol <nguyencaotan03072000@gmail.com>
2025-10-12 12:21:30 +0300

    Update nginx script

--- srcs/requirements/nginx/tools/nginxEntrypoint.sh
+++ srcs/requirements/nginx/tools/nginxEntrypoint.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 set -e
 
-# openssl req: create a SSL (Secure Sockets Layer certificate)
-#	-x509: create X.509 certificate.
-#	-days 365: valid for 365 days.
-#	-newkey rsa:2048: generate a new 2048-bit RSA key.
-#	-nodes: do not encrypt the private key.
-#	-keyout: output file for private key.
-#	-out: output file for certificate.
-#	-subj: set the certificate subject (Common Name) from $DOMAIN_NAME.
-openssl req -x509 \
+# openssl -> open tool work with ssl , req-> request ssl certi, -x509 -> option selfsigned -> get certi  immediately
+# -days 365 -> valid 365 days
+# create new pair key , rsa algorithm,2048 bits
+# nodes -> nginx read directly -> not require password
+#
+
+openssl req -x509 \ 
 			-days 365 \
 			-newkey rsa:2048 \
 			-nodes \
@@ -17,8 +15,7 @@ openssl req -x509 \
 			-out /etc/nginx/ssl/cert.crt \
 			-subj "/CN=$DOMAIN_NAME"
 
-# Set file permissions
-# Private key readable/writable only by owner (600)
+# Set file permissions -> key readable/writable only by owner (600)
 chmod 600 /etc/nginx/ssl/cert.key
 # Certificate readable by everyone (644)
 chmod 644 /etc/nginx/ssl/cert.crt
