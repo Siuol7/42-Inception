ed472892ac5deb034beb4f7b60135117546a9b27
Siuol <nguyencaotan03072000@gmail.com>
2025-10-23 00:27:30 +0300

    Update day 17

--- srcs/requirements/wordpress/tools/wordpressEntrypoint.sh
+++ srcs/requirements/wordpress/tools/wordpressEntrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 cd /var/www/html
 
+#Check if it is set up alr -> by checking flag file .firstmount
 if [ ! -e .firstmount ]; then
     echo "First time setup..."
     
@@ -13,16 +14,25 @@ if [ ! -e .firstmount ]; then
         WP_USER_PASSWORD="$(cat /run/secrets/wordpress_user_password)"
     fi
     
+    #Check if mariadb alive -> wait until mariadb ready 
+    #Connect to mariadb by tcp and hide all msgs and errors
     mariadb-admin ping --protocol=tcp --host=mariadb \
         -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" --wait >/dev/null 2>&1
     
+    #If wp-config.php exists -> Wordpress installed
     if [ ! -f wp-config.php ]; then
+
+        #using wp-cli tool to download core files -> always return true
         ./wp-cli.phar core download --allow-root || true
+
+        #create config file -> wordpress knows where to get database
         ./wp-cli.phar config create --allow-root \
             --dbname="$MYSQL_DB" \
             --dbuser="$MYSQL_USER" \
             --dbpass="$MYSQL_PASSWORD" \
             --dbhost="$MYSQL_HOST"
+
+        #Install (create tables + admin user in database)
         ./wp-cli.phar core install --allow-root \
             --skip-email \
             --url="$WP_URL" \
@@ -30,6 +40,8 @@ if [ ! -e .firstmount ]; then
             --admin_user="$WP_ADMIN" \
             --admin_password="$WP_ADMIN_PASSWORD" \
             --admin_email="$WP_ADMIN_EMAIL"
+        
+        
         ./wp-cli.phar user create --allow-root \
             "$WP_USER" "$WP_USER_EMAIL" \
             --role="$WP_USER_ROLE" \
